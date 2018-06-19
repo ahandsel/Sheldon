db1e40842a5fd71d3273b21d558a4022fe3b9099
Sheldon C <default_user@mcbk-3106.local>
2018-06-18 17:44:29 -0700

    Day of Week updates live

--- just js/dateToDayOfWeek.js
+++ just js/dateToDayOfWeek.js
@@ -3,12 +3,20 @@
     //Initialize the locale and thereby language of Moment.js
     moment.locale('en');
 
-    var SAVE_EVENTS = ['app.record.create.submit', 'app.record.edit.submit','app.record.index.edit.submit'];
+    // var SAVE_EVENTS = ['app.record.create.submit', 'app.record.edit.submit','app.record.index.edit.submit'];
+    var CHANGE_EVENTS = ['app.record.create.change.Date', 'app.record.edit.change.Date','app.record.index.edit.change.Date'];
     var EDIT_EVENTS = ['app.record.edit.show', 'app.record.create.show', 'app.record.index.edit.show'];
     
+    // //The "day of the week" field should be updated whenever a record is saved.
+    // kintone.events.on(SAVE_EVENTS, function(event) {
+    //     var date = event.record['Date'].value;
+    //     event.record['Day_Of_Week'].value = moment(date).format('dddd');
+    //     return event;
+    // });
+
     //The "day of the week" field should be updated whenever a record's date is changed.
-    kintone.events.on(SAVE_EVENTS, function(event) {
-        var date = event.record['Date'].value;
+    kintone.events.on(CHANGE_EVENTS, function(event) {
+        var date = event.changes.field.value;
         event.record['Day_Of_Week'].value = moment(date).format('dddd');
         return event;
     });
