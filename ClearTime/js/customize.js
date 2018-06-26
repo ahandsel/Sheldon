1d6d31a4160a562644418ea56307fdae8abec904
Sheldon C <default_user@mcbk-3106.local>
2018-06-26 10:39:51 -0700

    const named DELETE_KEY

--- ClearTime/js/customize.js
+++ ClearTime/js/customize.js
@@ -4,13 +4,14 @@
     var SHOW_EVENTS = ['app.record.create.show',
                        'app.record.edit.show',
                        'app.record.index.edit.show'];
+    var DELETE_KEY = 46;
 
     kintone.events.on(SHOW_EVENTS, function (e) {
       var timeFields = document.getElementsByClassName("input-time-text-cybozu");
       for (var i = 0; i < timeFields.length; i++) {
         timeFields[i].onkeydown = function(event) {
           var key = event.keyCode || event.charCode;
-          if (key == 8 || key == 46) {
+          if (key == DELETE_KEY) {
             this.value = "";
           }
         };
