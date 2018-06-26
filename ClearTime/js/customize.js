dfc4d46b2b2d1787f2069b6d96d025d84e174247
Sheldon C <default_user@mcbk-3106.local>
2018-06-26 10:45:15 -0700

    const named DELETE_KEY and BACKSPACE_KEY

--- ClearTime/js/customize.js
+++ ClearTime/js/customize.js
@@ -5,13 +5,14 @@
                        'app.record.edit.show',
                        'app.record.index.edit.show'];
     var DELETE_KEY = 46;
+    var BACKSPACE_KEY = 8;
 
     kintone.events.on(SHOW_EVENTS, function (e) {
       var timeFields = document.getElementsByClassName("input-time-text-cybozu");
       for (var i = 0; i < timeFields.length; i++) {
         timeFields[i].onkeydown = function(event) {
           var key = event.keyCode || event.charCode;
-          if (key == DELETE_KEY) {
+          if (key == DELETE_KEY || key == BACKSPACE_KEY) {
             this.value = "";
           }
         };
